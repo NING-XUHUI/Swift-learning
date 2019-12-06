9806d4db8e2f3789d737cfb1282c8d4d65fb5fc8
NING-XUHUI <892897986@qq.com>
2019-12-06 14:21:22 +0800

    编写规范

---
+++ 布尔类型.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+let hasLogined = true
+let hasRegistered = false
+
+hasLogined.hashValue
+hasRegistered.hashValue
+
+hasRegistered.description
+hasLogined.description
+
+let starCount = 12
+if starCount >= 10
+{
+    print("good")
+}
+
+
+
+
+
