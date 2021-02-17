97e10b04cb3881daa6e43cd2875a9593ec3ac52d
NING-XUHUI <1998ok4nxh@gmail.com>
2021-02-17 23:43:50 +0800

    Memorize

---
+++ Stanford/Developer/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 宁旭晖 on 2021/2/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
